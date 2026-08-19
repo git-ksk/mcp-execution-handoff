2d929ea28e0629b50adbe321d69ba8ba3dc38598
git-ksk <108925270+git-ksk@users.noreply.github.com>
2026-08-20 01:02:46 +0900

    experiment: add MTU-aware video packetizer

---
+++ experiments/thin-takeover-runtime/Sources/TakeoverCore/VideoPacket.swift
@@ -0,0 +1,159 @@
+import Foundation
+
+public struct VideoPacketHeader: Sendable, Equatable {
+    public static let magic: UInt32 = 0x54544B52 // TTKR
+    public static let version: UInt8 = 1
+    public static let encodedSize = 56
+
+    public var flags: UInt8
+    public var sessionHash: UInt64
+    public var epoch: UInt64
+    public var generation: UInt32
+    public var frameID: UInt64
+    public var packetIndex: UInt16
+    public var packetCount: UInt16
+    public var captureNanos: UInt64
+    public var encodeDoneNanos: UInt64
+
+    public init(
+        flags: UInt8 = 0,
+        sessionHash: UInt64,
+        epoch: UInt64,
+        generation: UInt32,
+        frameID: UInt64,
+        packetIndex: UInt16,
+        packetCount: UInt16,
+        captureNanos: UInt64,
+        encodeDoneNanos: UInt64
+    ) {
+        self.flags = flags
+        self.sessionHash = sessionHash
+        self.epoch = epoch
+        self.generation = generation
+        self.frameID = frameID
+        self.packetIndex = packetIndex
+        self.packetCount = packetCount
+        self.captureNanos = captureNanos
+        self.encodeDoneNanos = encodeDoneNanos
+    }
+
+    public func encode() -> Data {
+        var data = Data()
+        data.reserveCapacity(Self.encodedSize)
+        data.appendInteger(Self.magic)
+        data.append(Self.version)
+        data.append(flags)
+        data.appendInteger(UInt16(Self.encodedSize))
+        data.appendInteger(sessionHash)
+        data.appendInteger(epoch)
+        data.appendInteger(generation)
+        data.appendInteger(frameID)
+        data.appendInteger(packetIndex)
+        data.appendInteger(packetCount)
+        data.appendInteger(captureNanos)
+        data.appendInteger(encodeDoneNanos)
+        return data
+    }
+
+    public static func decode(_ data: Data) throws -> VideoPacketHeader {
+        guard data.count >= encodedSize else { throw VideoPacketError.truncated }
+        var cursor = DataCursor(data)
+        let receivedMagic: UInt32 = try cursor.readInteger()
+        guard receivedMagic == magic else { throw VideoPacketError.badMagic }
+        let receivedVersion = try cursor.readByte()
+        guard receivedVersion == version else { throw VideoPacketError.unsupportedVersion }
+        let flags = try cursor.readByte()
+        let headerLength: UInt16 = try cursor.readInteger()
+        guard headerLength == encodedSize else { throw VideoPacketError.invalidHeaderLength }
+        return VideoPacketHeader(
+            flags: flags,
+            sessionHash: try cursor.readInteger(),
+            epoch: try cursor.readInteger(),
+            generation: try cursor.readInteger(),
+            frameID: try cursor.readInteger(),
+            packetIndex: try cursor.readInteger(),
+            packetCount: try cursor.readInteger(),
+            captureNanos: try cursor.readInteger(),
+            encodeDoneNanos: try cursor.readInteger()
+        )
+    }
+}
+
+public enum VideoPacketError: Error, Equatable {
+    case truncated
+    case badMagic
+    case unsupportedVersion
+    case invalidHeaderLength
+}
+
+public struct VideoPacketizer: Sendable {
+    public let maxDatagramBytes: Int
+
+    public init(maxDatagramBytes: Int = 1200) {
+        precondition(maxDatagramBytes > VideoPacketHeader.encodedSize)
+        self.maxDatagramBytes = maxDatagramBytes
+    }
+
+    public func packetize(
+        payload: Data,
+        sessionHash: UInt64,
+        epoch: UInt64,
+        generation: UInt32,
+        frameID: UInt64,
+        captureNanos: UInt64,
+        encodeDoneNanos: UInt64,
+        keyframe: Bool
+    ) -> [Data] {
+        let maxPayload = maxDatagramBytes - VideoPacketHeader.encodedSize
+        let count = max(1, Int(ceil(Double(payload.count) / Double(maxPayload))))
+        precondition(count <= Int(UInt16.max))
+        return (0..<count).map { index in
+            let lower = index * maxPayload
+            let upper = min(payload.count, lower + maxPayload)
+            let header = VideoPacketHeader(
+                flags: keyframe ? 0x01 : 0,
+                sessionHash: sessionHash,
+                epoch: epoch,
+                generation: generation,
+                frameID: frameID,
+                packetIndex: UInt16(index),
+                packetCount: UInt16(count),
+                captureNanos: captureNanos,
+                encodeDoneNanos: encodeDoneNanos
+            )
+            var datagram = header.encode()
+            if upper > lower { datagram.append(payload.subdata(in: lower..<upper)) }
+            return datagram
+        }
+    }
+}
+
+private extension Data {
+    mutating func appendInteger<T: FixedWidthInteger>(_ value: T) {
+        var value = value.bigEndian
+        Swift.withUnsafeBytes(of: &value) { append(contentsOf: $0) }
+    }
+}
+
+private struct DataCursor {
+    let data: Data
+    var offset = 0
+
+    init(_ data: Data) { self.data = data }
+
+    mutating func readByte() throws -> UInt8 {
+        guard offset < data.count else { throw VideoPacketError.truncated }
+        defer { offset += 1 }
+        return data[offset]
+    }
+
+    mutating func readInteger<T: FixedWidthInteger>() throws -> T {
+        let width = MemoryLayout<T>.size
+        guard offset + width <= data.count else { throw VideoPacketError.truncated }
+        let slice = data[offset..<(offset + width)]
+        offset += width
+        var value: T = 0
+        for byte in slice { value = (value << 8) | T(byte) }
+        return value
+    }
+}
